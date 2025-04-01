cc2ca4896e23ba2473b5aa520172e8b2c38b90f4
BachaChor <daddycalling2u@gmail.com>
2025-04-01 17:00:15 +0530

    Add files via upload

---
+++ WebPage/Home_page/home_page.js
@@ -0,0 +1,10 @@
+let footerCourseElement = document.querySelector("#course-open-button");
+let navCourseElement = document.querySelector("#course-tab");
+
+navCourseElement.addEventListener("click", function() {
+    window.location.href = "../All_Courses_Tab/index.html";
+});
+
+footerCourseElement.addEventListener("click", function() {
+    window.location.href = "../All_Courses_Tab/index.html";
+});
